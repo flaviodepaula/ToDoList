ac2c9708503e4201985e489dabca18fe15ee5701
flavio <flavio@FLAVIONOTE>
2024-04-28 11:05:47 -0300

    feat: configurando swagger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,7 @@
 using Domain.Support;
 using Infra.Repository.Support;
 using WebApi.Authentication;
+using WebApi.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerConfiguration(builder.Configuration);
 builder.Services.AddJwtConfiguration(builder.Configuration);
 builder.Services.AddDomainExtensions(builder.Configuration);
 builder.Services.AddRepositoryExtensions(builder.Configuration);
