ec1ff3e1bc2140524f1e2a65643d56b3518229c8
flavio <flavio@FLAVIONOTE>
2024-04-27 23:23:49 -0300

    feat: criação das classes de Erro e Result

---
+++ Infra.Common/Errors/Error.cs
@@ -0,0 +1,24 @@
+﻿namespace Infra.Common.Errors
+{
+    public sealed class Error : IEquatable<Error>
+    {
+        public string Code { get; }
+        public string Message { get; }
+
+        public static readonly Error None = new(string.Empty, string.Empty);
+        public static readonly Error NullValue = new("Error.NullValue", "O Resultado retornado é nulo");
+
+        public static implicit operator string(Error error) => error.Code;
+
+        public Error(string code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+
+        public bool Equals(Error? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infra.Common/Result/Result.cs
@@ -0,0 +1,28 @@
+﻿using Infra.Common.Errors;
+
+namespace Infra.Common.Result
+{
+    public class Result
+    {
+        public bool IsSucess { get; }
+        public bool IsFailure => !IsSucess;
+        public Error Error { get; }
+        public static Result Sucess() => new(true, Error.None);
+        public static Result<T> Sucess<T>(T value) => new(value, true, Error.None);
+        public static Result Failure(Error error) => new(false, error);
+        public static Result<T> Failure<T>(Error error) => new(default, false, error);
+        public static Result<T> Failure<T>(Error error, T value) => new(value, false, error);
+
+        protected Result(bool isSucess, Error error)
+        {
+            if (isSucess && error != Error.None)
+                throw new InvalidOperationException();
+
+            if (!isSucess && error == Error.None)
+                throw new InvalidOperationException();
+
+            IsSucess = isSucess;
+            Error = error;
+        }
+    }
+}
