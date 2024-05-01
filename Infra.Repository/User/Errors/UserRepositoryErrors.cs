3f219fc1416429f671a325d515bdf5685e33c77b
flavio <flavio@FLAVIONOTE>
2024-05-01 08:36:26 -0300

    fix: traduzir erros para português

--- Infra.Repository/User/Errors/UserRepositoryErrors.cs
+++ Infra.Repository/User/Errors/UserRepositoryErrors.cs
@@ -6,19 +6,19 @@ namespace Infra.Repository.User.Errors
     {   
         public static Error UnableToCreateUser(string errorMessage, string innerException)
         {
-            return new("Repository.Users.Errors.UnableToCreateUser", $"Unable to create User. ErrorMessage: {errorMessage}. InnerException: {innerException}");
+            return new("Repository.Users.Errors.UnableToCreateUser", $"Não foi possível criar o Usuário. ErrorMessage: {errorMessage}. InnerException: {innerException}");
         }
 
         public static Error UnableToGetUsers(string errorMessage, string innerException)
         {
-            return new("Repository.Users.Errors.UnableToGetUsers", $"Unable to get Users. ErrorMessage: {errorMessage}. InnerException: {innerException}");
+            return new("Repository.Users.Errors.UnableToGetUsers", $"Não foi possível recueprar as informações dos usuários. ErrorMessage: {errorMessage}. InnerException: {innerException}");
         }
 
         public static Error GenericErrorOnRetrievingData(string errorMessage, string innerException)
         {
-            return new("Repository.Users.Errors.GenericErrorOnRetrievingData", $"An error occurred on request to database. ErrorMessage: {errorMessage}. InnerException: {innerException}");
+            return new("Repository.Users.Errors.GenericErrorOnRetrievingData", $"Erro ao efetuar requisição para o banco de dados. ErrorMessage: {errorMessage}. InnerException: {innerException}");
         }
 
-        public static readonly Error UserDoesNotExist = new("Repository.Users.Errors.UserDoesNotExist", $"User required does not exist. ");        
+        public static readonly Error UserDoesNotExist = new("Repository.Users.Errors.UserDoesNotExist", $"Usuário pesquisado não existe. ");        
     }
 }
